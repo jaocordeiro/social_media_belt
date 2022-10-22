2bb327f333e434a3215cf008de9c7d6f58a86942
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-10-22 18:00:09 -0300

    feat: render layout of each route specifies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import LayoutApp from '@/components/Layout/LayoutApp';
+import LayoutPublic from '@/components/Layout/LayoutPublic';
+import LayoutTenant from '@/components/Layout/LayoutTenant';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { pathname } = router;
+  let Layout = LayoutPublic;
+  if (pathname.indexOf('/app') === 0) {
+    Layout = LayoutApp;
+  }
+  if (pathname.indexOf('/[slug]') === 0) {
+    Layout = LayoutTenant;
+  }
+  return (
+    <Layout>
+      <pre>{JSON.stringify(pathname)}</pre>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
